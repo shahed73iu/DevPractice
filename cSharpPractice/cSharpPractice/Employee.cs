867097f3c285b7b2b8bb12ae82b508aced306548
Md. Emam Uddin Shahed <shahedimamuddin@gmail.com>
2021-01-11 01:25:00 +0600

    class one everything done

--- cSharpPractice/cSharpPractice/Employee.cs
+++ cSharpPractice/cSharpPractice/Employee.cs
@@ -2,9 +2,26 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Engineering
+namespace cSharpPractice
 {
-    class Employee
+    public class Employee
     {
+        public int x;
+        private int y; /// Private "Y" only ei employee class ei bebohar kora jabe onno kono class e impossible 
+        protected int z; /// eta k employee class er child class e eta k pabo amra
+        internal int a; /// Same assembly te thakle pabo noile pabona
+        public void CalculateSalary(int bonus, int age)
+        {
+            int t = a + x + y + z; /// Employee Class er moddhe tai shob variable kei paitesi 
+                                   /// Program Class e gele only "x" & "a" ke pabo others gula pabona 
+        }
+    }
+    public class SpecialEmployee : Employee
+    {
+        public void DoSomething()
+        {
+            int g = a + x + z ; // "Y" is not accessable 
+        }
+
     }
 }
--- cSharpPractice/cSharpPractice/Program.cs
+++ cSharpPractice/cSharpPractice/Program.cs
@@ -2,7 +2,7 @@
 
 namespace cSharpPractice
 {
-    public class Program
+    public class Program : Employee
     {
         static void Main(string[] args)
         {
@@ -24,21 +24,48 @@ namespace cSharpPractice
             s1.name = "shahed";
             s1.age = 25;
 
-            Student s2 = new Student();
+            //Student s2 = new Student();
+            Student s2 = s1;
             s2.name = "shajid";
             s2.age = 21;
 
-            Console.WriteLine(");
+            Student s3 = s2;
 
+            Console.WriteLine("Student 1 Name : {0} , Student 1 Age : {1}",s1.name , s1.age);
+            Console.WriteLine("Student 2 Name : {0} , Student 2 Age : {1}",s2.name , s2.age);
+            Console.WriteLine("Student 3 Name : {0} , Student 3 Age : {1}",s3.name , s3.age);
 
+            Point point = new Point();
+            point.x = 10;
+            point.y = 20;
+
+            Point point2 = new Point();
+            point2.x = 40;
+            point2.y = 50;
+
+            Console.WriteLine("Point 1 X: {0} Point 1 Y: {1}", point.x , point.y);
+            Console.WriteLine("Point 2 X: {0} Point 2 Y: {1}",point2.x, point2.y);
+
+            Employee employee = new Employee();
+            employee.x = 10;
+            employee.a = 20; /// a is internal Type variable, 
+        }
+        public void DoNothing()
+        {
+            Employee employee1 = new Employee();
+            employee1.x = 88;
+            employee1.a = 66;
+            ///employee1.z
+            z = 10;
+            Console.WriteLine(z);
         }
     }
-    struct point
+    struct Point
     {
         public int x;
         public int y;
     }
-    class Student
+    class Student 
     {
         public string name;
         public int age;
