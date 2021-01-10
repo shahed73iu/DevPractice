b49d5306b71f59a8a5a7303e5bf87a46fbc81115
Md. Emam Uddin Shahed <shahedimamuddin@gmail.com>
2021-01-10 23:49:35 +0600

    Class-1 Struct vs Class , nameSpace, dataType  Value Type Reference type

---
+++ cSharpPractice/cSharpPractice/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace cSharpPractice
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;            // This All Are Value Type
+            char b;          // This All Are Value Type
+            short c;          // This All Are Value Type
+            long d;         // This All Are Value Type
+            DateTime xxxxe;    // This All Are Value Type
+            float f;          // This All Are Value Type
+            double g;         // This All Are Value Type
+            uint h;          // This All Are Value Type
+            bool i;          // This All Are Value Type
+            decimal j;       // This All Are Value Type
+
+            string k;  /// only string ta holo reference type 
+
+            //Making Student Class
+            Student s1 = new Student();
+            s1.name = "shahed";
+            s1.age = 25;
+
+            Student s2 = new Student();
+            s2.name = "shajid";
+            s2.age = 21;
+
+            Console.WriteLine(");
+
+
+        }
+    }
+    struct point
+    {
+        public int x;
+        public int y;
+    }
+    class Student
+    {
+        public string name;
+        public int age;
+    }
+}
