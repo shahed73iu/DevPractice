d04154da27b024a3e2283aabd43068fb46f1b025
Md. Emam Uddin Shahed <shahedimamuddin@gmail.com>
2021-01-13 00:47:55 +0600

    New Project Added For Abstract CLass and Interface

---
+++ cSharpPractice/AbstractClassVsInterface1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace AbstractClassVsInterface1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var missile = new Missile();
+            // var machineGun = new MachineGun();
+            // missile.Fire();
+            // machineGun.Fire();
+            Weapon[] weapons= new Weapon[2];
+            weapons[0] = new Missile();
+            weapons[1] = new MachineGun();
+            //for (int i = 0; i < weapons.Length; i++)
+            //{
+            //    weapons[i].Fire();
+            //}
+
+            foreach (var weapon in weapons)
+            {
+                weapon.Fire();
+            }
+        }
+    }
+    public interface IWeapon
+    {
+
+    }
+    public abstract class Weapon
+    {
+        protected int DamagePower;
+        public abstract void Fire();
+    }
+    public class Missile : Weapon
+    {
+        public Missile()
+        {
+            DamagePower = 200;
+        }
+        public override void Fire()
+        {
+            Console.WriteLine("Missile was fired with damage amount: {0}", DamagePower);
+        }
+    }
+    public class MachineGun : Weapon
+    {
+        public MachineGun()
+        {
+            DamagePower = 100;
+        }
+        public override void Fire()
+        {
+            Console.WriteLine("MachineGun was fired with damage amount: {0}", DamagePower);
+        }
+    }
+
+
+}
